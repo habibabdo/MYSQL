980d557e0d5fac78c2ec9418627149423a2331f7
habibabdo <habibdesigner9971728@gmail.com>
2020-06-15 13:17:16 +0300

    select changed

--- Select.py
+++ Select.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host='localhost', user='root', password='1234', database='salon')
+mydb = mysql.connector.connect(host='localhost', user='root', password='1234', database='habib')
 
 mycursor = mydb.cursor()
 
-mycursor.execute('SELECT * FROM contacts')
+mycursor.execute('SELECT * FROM student')
 
 myresult = mycursor.fetchall()
 
