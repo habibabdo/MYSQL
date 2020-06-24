fd9842b32a6a7fc26bd7b60162150c001afa617a
habibabdo <habibdesigner9971728@gmail.com>
2020-06-24 19:03:42 +0300

    COUNT.py  changed

--- COUNT.py
+++ COUNT.py
@@ -20,3 +20,20 @@ if myresult:
 else:
     print('No Records In Data Base')
 
+mycursor = mydb.cursor()
+
+mycursor.execute('SELECT * FROM  flights WHERE id IN ( SELECT  id FROM passengers GROUP BY flight_id HAVING  COUNT(*) > 1)')
+
+myresult = mycursor.fetchall()
+
+if myresult:
+
+    print('COUNT  Have Records:')
+    print('FLIGHT ID')
+    print('_______________')
+    for x in myresult:
+        print(x)
+
+
+else:
+    print('No Records In Data Base')
