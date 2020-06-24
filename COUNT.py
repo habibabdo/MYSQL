23d6cf8a72f92d83e350537620c500d2dcc599be
habibabdo <habibdesigner9971728@gmail.com>
2020-06-24 18:45:20 +0300

    Count CREATED

---
+++ COUNT.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host='localhost', user='root', password='', database='salon')
+
+mycursor = mydb.cursor()
+
+mycursor.execute('select flight_id  FROM  passengers GROUP BY flight_id HAVING COUNT(*) > 1')
+
+myresult = mycursor.fetchall()
+
+if myresult:
+
+    print('COUNT  Have Records:')
+    print('FLIGHT ID')
+    print('_______________')
+    for x in myresult:
+        print(x[0])
+    # print('</td></tr></table>')
+
+else:
+    print('No Records In Data Base')
+
